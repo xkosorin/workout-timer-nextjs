34c8307d9842f4e474accf1f8d63b5be82d9e4d1
Kristián <kristian@kosorin.eu>
2022-07-07 20:38:35 +0200

    Added passwordless email login

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { NextApiHandler } from "next";
 import NextAuth from "next-auth";
 import GitHubProvider from 'next-auth/providers/github';
+import EmailProvider from "next-auth/providers/email";
 import prisma from "../../../lib/prisma";
 
 const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
@@ -13,8 +14,11 @@ const options = {
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM
+    }),
   ],
-
   adapter: PrismaAdapter(prisma),
   secret: process.env.SECRET,
 };
