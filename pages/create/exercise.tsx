ca74adca10d29ad479293cbc66157f2758e0daae
Kristián <kristian@kosorin.eu>
2022-07-07 09:25:58 +0200

    Added pages to create exercise and workout

--- pages/create/exercise.tsx
+++ pages/create/exercise.tsx
@@ -24,38 +24,24 @@ const CreateExercise: React.FC = () => {
     <div>
       <form onSubmit={submitData} >
         <h2>Add new exercise to database</h2>
-        <input autoFocus onChange={handleInput} type="text" placeholder="Exercise title" id="title" name="title" />
-        <textarea onChange={handleInput} cols={30} placeholder="Exercise description" rows={3} id="description" name="description" />
-        <input onChange={handleInput} type="url" placeholder="Exercise image / vide URL" id="mediaURL" name="mediaURL" />
-        <input onChange={handleInput} type="checkbox" id="mediaIsImage" name="mediaIsImage" />
-        <label htmlFor="mediaIsImage">Is media image?</label><br />
-        <div>
-          <input disabled={!data.title} type="submit" value="Create" />
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="title">Exercise title</label>
+          <input autoFocus onChange={handleInput} type="text" placeholder="Exercise title" id="title" name="title" />
         </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="description">Exercise description</label>
+          <textarea onChange={handleInput} cols={30} placeholder="Exercise description" rows={3} id="description" name="description" />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="mediaURL">Link for image / video</label>
+          <input onChange={handleInput} type="url" placeholder="Exercise image / vide URL" id="mediaURL" name="mediaURL" />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="mediaIsImage">Is media image?</label>
+          <input onChange={handleInput} type="checkbox" id="mediaIsImage" name="mediaIsImage" />
+        </div>
+        <input disabled={!data.title} type="submit" value="Create" />
       </form>
-      <style jsx>{`
-        .page {
-          padding: 3rem;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        input[type='text'],
-        textarea {
-          width: 100%;
-          padding: 0.5rem;
-          margin: 0.5rem 0;
-          border-radius: 0.25rem;
-          border: 0.125rem solid rgba(0, 0, 0, 0.2);
-        }
-
-        input[type='submit'] {
-          background: #ececec;
-          border: 0;
-          padding: 1rem 2rem;
-        }
-      `}</style>
     </div>
   )
 }
