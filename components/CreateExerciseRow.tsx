100bf82d10d29a384afcef3a6e07818959454e4c
Kristian Kosorin <kristian@kosorin.eu>
2022-07-22 12:20:22 +0200

    Created ExercsieRow component and implement adding in to table at create workout page

---
+++ components/CreateExerciseRow.tsx
@@ -0,0 +1,69 @@
+import { SyntheticEvent, useEffect, useState } from 'react';
+
+type Props = {
+  id: string;
+  title: string;
+  onUpdate: Function;
+  onDelete: Function;
+}
+
+const CreateExerciseRow: React.FC<Props> = (props: Props) => {
+  const [reps, setReps] = useState(0);
+  const [timed, setTimed] = useState(false);
+
+  const updateReps = (e:SyntheticEvent, direction: string) => {
+    if (direction === "plus") {
+      setReps(reps + 1)
+      return;
+    }
+    
+    if (direction === "minus" && reps > 0)
+      setReps(reps - 1);
+  }
+
+  const handleInput = (e:SyntheticEvent) => {
+    setReps(Number((e.target as HTMLInputElement).value));
+  }
+
+  const handleToggle = (e:SyntheticEvent) => {
+    setTimed((e.target as HTMLInputElement).checked);
+  }
+
+  useEffect(
+    () => {
+      props.onUpdate(props.id, reps, timed)
+    },
+    [reps, timed]
+  )
+
+  return(
+    <tr className="w-full border-b">
+      <td className="py-6 pl-4">{ props.title }</td>
+      <td className="text-center">                    
+        <div className="xt-list flex-nowrap justify-center" data-xt-groupnumber>
+          <div className="inline-flex">
+            <button onClick={ e => updateReps(e, "minus") } className=" bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer outline-none">
+                <span className="m-auto text-2xl font-thin">-</span>
+            </button>
+            <input type="number" value={ reps } className="outline-none focus:outline-none text-center w-full bg-gray-300 font-semibold text-md hover:text-black focus:text-black md:text-basecursor-default flex items-center text-gray-700" name="custom-input-number" onInput={ handleInput } min="0" />
+            <button onClick={ e => updateReps(e, "plus") } className="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer">
+                <span className="m-auto text-2xl font-thin">+</span>
+            </button>
+          </div>
+        </div>
+      </td>
+      <td className="text-center">
+      <input
+            type="checkbox"
+            aria-label="Amount of reps or seconds"
+            className="xt-check xt-switch rounded-full bg-gray-200 border border-transparent transition-all checked:bg-primary-500"
+            name="switch-usage"
+            onChange={ handleToggle }
+        />
+      </td>
+      <td className="text-center"><button className="border px-1 rounded-full text-red-600 bg-white border-red-800 text-lg leading-5 align-text-top" onClick={ () => props.onDelete(props.id) }>&times;</button></td>
+    </tr>
+  );
+}
+
+export default CreateExerciseRow;
--- pages/create/workout.tsx
+++ pages/create/workout.tsx
@@ -1,41 +1,106 @@
-import { NextPage } from "next";
-import React, { useState } from "react";
+import { GetServerSideProps, NextPage } from "next";
+import React, { SyntheticEvent, useState } from "react";
 import Layout from "../../components/Layout";
 import 'xtendui';
 import 'xtendui/src/groupnumber';
-import LapTable from "../../components/LapTable";
+import CreateExerciseRow from "../../components/CreateExerciseRow";
+import prisma from "../../lib/prisma";
+import { getSession } from "next-auth/react";
+import { v4 as uuid } from 'uuid';
 
-const CreateWorkout: NextPage = () => {
+type Laps = {
+  laps: UsedExercise[];
+}
+
+type UsedExercise = {
+  uuid: string;
+  exerciseId: string;
+  title: string;
+  reps: number;
+  timed: boolean;
+}
+
+type Exercise = {
+  id: string;
+  title?: string;
+}
+
+type Props = {
+  exerciseList: Exercise[];
+}
+
+let defaultData: UsedExercise[] = [
+  {uuid: uuid(), title: "Squat", exerciseId: "cl5uwpkb80097jotq46rpt1gy", reps: 0, timed: false},
+  {uuid: uuid(), title: "Front squat", exerciseId: "cl5uwpkb80dd097jotq46rpt1gy", reps: 0, timed: false}
+]
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const session = await getSession({ req });
+  if (!session) {
+    res.statusCode = 403;
+    return { props: { exerciseList: [] } };
+  }
+
+  const exerciseList = await prisma.exercise.findMany();
+
+  return { props: { exerciseList: JSON.parse(JSON.stringify(exerciseList)) } };
+}
+
+const CreateWorkout: NextPage<Props> = (props) => {
   const [showSelect, setShowSelect] = useState(false);
-  const [lapsAmount, setLapsAmount] = useState(0);
-  const [data, setData] = useState({});
+  const [selectedExercise, setSelectedExercise] = useState<Exercise | null>(null);
+  const [data, setData] = useState(defaultData);
 
-  const submitData = async(e: React.SyntheticEvent) => {
+  const submitData = async(e: SyntheticEvent) => {
     e.preventDefault();
   }
 
-  const handleInput = (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {
+  const handleUpdate = (id: string, reps: number, timed: boolean) => {
+    const newState = data.map(data => {
+      if (data.uuid === id) 
+        return {...data, reps, timed};
+
+      return data;
+    });
+
+    setData(newState);
+  }
+
+  const handleTitleUpdate = (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {
     let name = e.currentTarget.name;
     let value = e.currentTarget.type === "switch-usage" ? (e.currentTarget as HTMLInputElement).checked : e.currentTarget.value;
-    console.log(name);
-    console.log(value);
   }
 
-  const handleRemove = (e: React.SyntheticEvent) => {
-    e.preventDefault();
-    console.log(e);
+  const handleSelect = (e: SyntheticEvent) => {
+    setSelectedExercise({
+      id: (e.target as HTMLInputElement).value,
+      title: props.exerciseList.find(exercise => exercise.id === (e.target as HTMLInputElement).value)?.title
+    });
   }
 
-  const handleAddExerciseClick = (e: React.SyntheticEvent) => {
+  const handleAddExerciseClick = (e: SyntheticEvent) => {
     e.preventDefault();
-
     setShowSelect(true);
+
+    if (selectedExercise === null)
+      return;
+
+    const newExercise: UsedExercise = {
+      uuid: uuid(),
+      exerciseId: selectedExercise.id,
+      title: selectedExercise.title ?? "Unknown exercise",
+      reps: 0,
+      timed: false
+    }
+
+    setData([...data, newExercise])
   }
 
-  let exercises = [
-    {title: "Squat", amount: 0, isTimed: false},
-    {title: "Front squat", amount: 5, isTimed: true}
-  ]
+  const handleDelete = (id: string) => {
+    setData((prevData) =>
+      prevData.filter((prevItem) => prevItem.uuid !== id)
+    );
+  }
 
   return (
     <Layout>
@@ -43,83 +108,50 @@ const CreateWorkout: NextPage = () => {
         <h2 className="p-10 pl-0 text-xl">Create new workout</h2>
         <div className="mb-4">
           <label className="input-label" htmlFor="title">Workout title</label>
-          <input className="input-text" autoFocus onChange={handleInput} type="text" placeholder="Exercise title" id="title" name="title" />
+          <input className="input-text" autoFocus onChange={handleTitleUpdate} type="text" placeholder="Exercise title" id="title" name="title" />
         </div>
-          <div className="mb-4">
-            <label className="input-label" htmlFor="title">Exercises in lap #</label>
-            <div className="block w-full bg-gray-50 text-gray-700 border border-slate-300 rounded py-3 mb-3 leading-tight">
-              <table className="w-full">
-                <thead className="text-xs border-b border-slate-300">
-                  <tr>
-                    <th className="w-8/12 text-left pl-4 pb-4">Exercise</th>
-                    <th className="w-1/12 text-center pb-4">Reps. / Sec.</th>
-                    <th className="w-1/12 text-center pb-4">Is timed</th>
-                    <th className="w-2/12 text-center pb-4">Action</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr className="border-b border-slate-300">
-                    <td className="py-6 pl-4">Squat</td>
-                    <td className="text-center">
-                      <div className="xt-list flex-nowrap" data-xt-groupnumber>
-                        <div className="inline-flex">
-                          <button
-                            type="button"
-                            className="xt-button text-2xs py-1 px-2.5 rounded-md rounded-r-none border border-r-0 border-gray-300 font-medium leading-snug tracking-wider uppercase text-gray-700 bg-gray-100 transition hover:bg-gray-200 active:bg-gray-300 on:bg-gray-200"
-                            data-xt-step="-1">
-                            -
-                          </button>
-                          <input
-                            type="number"
-                            className="block w-16 text-center border border-gray-300 py-2.5 px-3.5 text-gray-700 placeholder-black placeholder-opacity-75 bg-gray-100 transition focus:bg-gray-200 focus:outline-none"
-                            aria-label="Quantity"
-                            defaultValue="0"
-                            min="0"
-                          />
-                          <button
-                            type="button"
-                            className="xt-button text-2xs py-1 px-2.5 rounded-md rounded-l-none border border-l-0 border-gray-300 font-medium leading-snug tracking-wider uppercase text-gray-700 bg-gray-100 transition hover:bg-gray-200 active:bg-gray-300 on:bg-gray-200"
-                            data-xt-step="1">
-                            +
-                          </button>
-                        </div>
+        <div className="mb-4">
+          <label className="input-label" htmlFor="title">Exercises in lap #</label>
+          <div className="block w-full">
+            <table className="w-full bg-gray-50 text-gray-700 border border-slate-300 py-3 mb-3 leading-tight">
+              <thead className="text-xs border-b border-slate-300 rounded-tl-lg rounded-tr-lg">
+                <tr>
+                  <th className="w-8/12 text-left pl-4 py-4">Exercise</th>
+                  <th className="w-1/12 text-center py-4">Reps. / Sec.</th>
+                  <th className="w-1/12 text-center py-4">Is timed</th>
+                  <th className="w-2/12 text-center py-4">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((exercise) => (
+                  <CreateExerciseRow id={ exercise.uuid } title={ exercise.title } onUpdate={ handleUpdate } onDelete={ handleDelete } key={ exercise.uuid } />
+                ))}
+              </tbody>
+              <tfoot>
+                <tr>
+                  <td className="pt-4 pl-4">
+                    {
+                      showSelect &&
+                      <div className="inline-block mr-5">
+                        <select
+                          className="inline-block w-full xt-select rounded-md py-2.5 px-3.5 pr-20 text-gray-900 placeholder-black placeholder-opacity-75 bg-gray-100 transition focus:bg-gray-200 focus:outline-none"
+                          aria-label="Select"
+                          onChange={handleSelect}>
+                          <option defaultValue="">Select an option</option>
+                          {props.exerciseList.map((exercise) => (
+                            <option value={ exercise.id } key={ exercise.id }>{ exercise.title }</option>
+                          ))}
+                        </select>
                       </div>
-                    </td>
-                    <td className="text-center">
-                      <input
-                        type="checkbox"
-                        aria-label="Amount of reps or seconds"
-                        className="xt-check xt-switch rounded-full bg-gray-200 border border-transparent transition-all checked:bg-primary-500"
-                        name="switch-usage"
-                      />
-                    </td>
-                    <td className="text-center"><button>Remove</button></td>
-                  </tr>
-                </tbody>
-                <tfoot>
-                  <tr>
-                    <td className="pt-4 pl-4">
-                      {
-                        showSelect &&
-                        <div className="inline-block mr-5">
-                          <select
-                            className="inline-block w-full xt-select rounded-md py-2.5 px-3.5 pr-20 text-gray-900 placeholder-black placeholder-opacity-75 bg-gray-100 transition focus:bg-gray-200 focus:outline-none"
-                            aria-label="Select">
-                            <option defaultValue="">Select an option</option>
-                            <option>Test</option>
-                            <option>Test</option>
-                            <option>Test</option>
-                          </select>
-                        </div>
-                      }
-                      <button onClick={handleAddExerciseClick}>Add { !showSelect && "exercise"}</button>
-                    </td>
-                  </tr>
-                </tfoot>
-              </table>
-              <LapTable exercises={exercises} onChange={handleInput} onRemove={handleRemove} />
-            </div>
+                    }
+                    <button onClick={handleAddExerciseClick} disabled={(showSelect && selectedExercise === null)}>Add { !showSelect && "exercise"}</button>
+                  </td>
+                </tr>
+              </tfoot>
+            </table>
           </div>
+        </div>
+        <button className="">Add lap</button>
       </form>
     </Layout>
   );
