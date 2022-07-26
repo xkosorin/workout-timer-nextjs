935390ec98e542fece6c7fe9a69ebda64de2b2d1
Kristian Kosorin <kristian@kosorin.eu>
2022-07-26 11:08:23 +0200

    Changed list exercises page to use global types

--- pages/exercises.tsx
+++ pages/exercises.tsx
@@ -2,12 +2,10 @@ import { GetServerSideProps, NextPage } from "next";
 import Image from "next/image";
 import Layout from "../components/Layout";
 import prisma from '../lib/prisma'
+import { Exercise } from "../types";
 
-type ThisExerc = {
-  id: string;
-  title: string;
-  description: string | null;
-  mediaURL: string | null;
+type Props = {
+  exercises: Exercise[]
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
@@ -28,17 +26,17 @@ export const getServerSideProps: GetServerSideProps = async () => {
 }
 
 
-const Exercises: NextPage<{ exercises: ThisExerc[] }> = (props: { exercises: ThisExerc[] }) => {
+const Exercises: NextPage<Props> = (props: Props) => {
   return(
     <Layout>
       <div className="grid grid-cols-3 gap-4">
       {
-        props.exercises.map((single: ThisExerc, i: any) => {
+        props.exercises.map((single: Exercise, i: number) => {
           return (
-            <div key={i} className="flex flex-col items-center">
-              <h3 key={i} className="uppercase text-lg text-gray-600 font-semibold flex-1">{single.title}</h3>
-              <p className="text-sm font-light text-slate-400">{single.description}</p>
-              {single.mediaURL ? <Image src={single.mediaURL} width={300} height={300} alt="Picture of the author" className="flex-1"/> : ""}
+            <div key={ i } className="flex flex-col items-center">
+              <h3 key={ i } className="uppercase text-lg text-gray-600 font-semibold flex-1">{ single.title }</h3>
+              <p className="text-sm font-light text-slate-400">{ single.description }</p>
+              { single.mediaURL ? <Image src={ single.mediaURL } width={ 300 } height={ 300 } alt="Picture of the author" className="flex-1"/> : "" }
             </div>
           )
         })
