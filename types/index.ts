26f5a9464ee81f6393ebfa10bda1ecd002fb8235
Kristian Kosorin <kristian@kosorin.eu>
2022-07-26 11:06:57 +0200

    Unified typescript types into separate file

---
+++ types/index.ts
@@ -0,0 +1,34 @@
+import { User } from "@prisma/client";
+
+export type Lap = {
+  id: string;
+  exercises: UsedExerciseArrayItem[];
+  exerciseCount: number;
+}
+
+export type UsedExerciseArrayItem = {
+  usedExercise: UsedExercise
+}
+
+export type UsedExercise = {
+  id: string;
+  exercise: Exercise;
+  reps: number;
+  timed: boolean;
+}
+
+export type Exercise = {
+  id?: string;
+  title: string;
+  description?: string;
+  mediaURL?: string;
+  mediaIsImage?: boolean;
+}
+
+export type Workout = {
+  id: string;
+  title: string;
+  description: string;
+  laps: Lap[];
+  user: User;
+}
