daad68cc21860da3f8186fa7799eb9e8897f16de
Kristian Kosorin <kristian@kosorin.eu>
2023-02-15 10:54:46 +0100

    Updated package.json & next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
 module.exports = {
   images: {
-    domains: ['image.shutterstock.com'],
+    domains: ["image.shutterstock.com", "www.shutterstock.com"],
   },
-}
+};
