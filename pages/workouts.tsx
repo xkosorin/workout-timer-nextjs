80686d0c884189ad337c5734b25247948d2ec1d8
Kristian Kosorin <kristian@kosorin.eu>
2023-02-17 11:17:50 +0100

    Fix bug on workouts page

--- pages/workouts.tsx
+++ pages/workouts.tsx
@@ -2,42 +2,51 @@ import { GetServerSideProps, GetServerSidePropsContext, NextPage } from "next";
 import { getSession } from "next-auth/react";
 import Link from "next/link";
 import Layout from "../components/Layout";
-import prisma from '../lib/prisma'
+import prisma from "../lib/prisma";
 import { Workout } from "../types";
 
 type Props = {
-  workouts: Workout[]
-}
+  workouts: Workout[];
+};
 
 const Workouts: NextPage<Props> = (props: Props) => {
-  return(
+  return (
     <Layout>
       <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
-      {
-        props.workouts.map((workout: Workout, i: number) => {
+        {props.workouts.map((workout: Workout, i: number) => {
           return (
-            <div key={ i } className="flex flex-col items-center">
-              <Link href={ "/workout/" + workout.id }><h3 key={ i } className="uppercase text-lg text-gray-600 font-semibold flex-1">{ workout.title }</h3></Link>
-              <p className="text-sm font-light text-slate-400">{ workout.description }</p>
+            <div key={i} className="flex flex-col items-center">
+              <Link href={"/workout/" + workout.id}>
+                <h3
+                  key={i}
+                  className="uppercase text-lg text-gray-600 font-semibold flex-1"
+                >
+                  {workout.title}
+                </h3>
+              </Link>
+              <p className="text-sm font-light text-slate-400">
+                {workout.description}
+              </p>
             </div>
-          )
-        })
-      }
+          );
+        })}
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export const getServerSideProps: GetServerSideProps = async (context: GetServerSidePropsContext) => {
-  const session = await getSession(context)
+export const getServerSideProps: GetServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const session = await getSession(context);
 
   if (!session) {
     return {
       redirect: {
-        destination: '/404',
+        destination: "/404",
         permanent: false,
       },
-    }
+    };
   }
 
   const res = await prisma.workout.findMany({
@@ -46,15 +55,15 @@ export const getServerSideProps: GetServerSideProps = async (context: GetServerS
       title: true,
       description: true,
       laps: true,
-      user: true
-    }
+      userId: true,
+    },
   });
 
   return {
     props: {
-      workouts: JSON.parse(JSON.stringify(res))
-    }
+      workouts: JSON.parse(JSON.stringify(res)),
+    },
   };
-}
+};
 
 export default Workouts;
