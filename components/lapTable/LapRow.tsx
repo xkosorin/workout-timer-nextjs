0b7119e3294ab8d512cba4d25c3b68eaed5fd09e
Kristian Kosorin <kristian@kosorin.eu>
2023-02-20 11:36:09 +0100

    Fixed bug in while editing workout

--- components/lapTable/LapRow.tsx
+++ components/lapTable/LapRow.tsx
@@ -1,4 +1,4 @@
-import { FC, SyntheticEvent, useEffect, useState } from 'react';
+import { FC, SyntheticEvent, useEffect, useState } from "react";
 
 type Props = {
   id: string;
@@ -6,8 +6,8 @@ type Props = {
   reps: number;
   timed: boolean;
   onUpdate(id: string, reps: number, timed: boolean): void;
-  onDelete(id: string): void;
-}
+  onDelete(e: SyntheticEvent, id: string): void;
+};
 
 const LapRow: FC<Props> = (props: Props) => {
   const [reps, setReps] = useState(props.reps);
@@ -15,63 +15,82 @@ const LapRow: FC<Props> = (props: Props) => {
 
   const updateReps = (e: SyntheticEvent, direction: string) => {
     e.preventDefault();
-    
+
     if (direction === "plus") {
-      setReps(reps + 1)
+      setReps(reps + 1);
       return;
     }
-    
-    if (direction === "minus" && reps > 0)
-      setReps(reps - 1);
-  }
 
-  const handleInput = (e:SyntheticEvent) => {
+    if (direction === "minus" && reps > 0) setReps(reps - 1);
+  };
+
+  const handleInput = (e: SyntheticEvent) => {
     e.preventDefault();
 
     setReps(Number((e.target as HTMLInputElement).value));
-  }
+  };
 
-  const handleToggle = (e:SyntheticEvent) => {
+  const handleToggle = (e: SyntheticEvent) => {
     setTimed((e.target as HTMLInputElement).checked);
-  }
+  };
 
   useEffect(
     () => {
-      props.onUpdate(props.id, reps, timed)
+      props.onUpdate(props.id, reps, timed);
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [reps, timed]
-  )
+  );
 
-  return(
+  return (
     <tr className="w-full border-b">
-      <td className="py-6 pl-4 font-medium">{ props.title }</td>
-      <td className="text-center">                    
+      <td className="py-6 pl-4 font-medium">{props.title}</td>
+      <td className="text-center">
         <div className="xt-list flex-nowrap justify-center" data-xt-groupnumber>
           <div className="inline-flex">
-            <button onClick={ e => updateReps(e, "minus") } className=" bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer outline-none">
-                <span className="m-auto text-2xl font-thin">-</span>
+            <button
+              onClick={(e) => updateReps(e, "minus")}
+              className=" bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer outline-none"
+            >
+              <span className="m-auto text-2xl font-thin">-</span>
             </button>
-            <input type="number" value={ reps } className="outline-none focus:outline-none text-center w-full bg-gray-300 font-semibold text-md hover:text-black focus:text-black md:text-basecursor-default flex items-center text-gray-700 appearance-none" name="custom-input-number" onInput={ handleInput } min="0" />
-            <button onClick={ e => updateReps(e, "plus") } className="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer">
-                <span className="m-auto text-2xl font-thin">+</span>
+            <input
+              type="number"
+              value={reps}
+              className="outline-none focus:outline-none text-center w-full bg-gray-300 font-semibold text-md hover:text-black focus:text-black md:text-basecursor-default flex items-center text-gray-700 appearance-none"
+              name="custom-input-number"
+              onInput={handleInput}
+              min="0"
+            />
+            <button
+              onClick={(e) => updateReps(e, "plus")}
+              className="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer"
+            >
+              <span className="m-auto text-2xl font-thin">+</span>
             </button>
           </div>
         </div>
       </td>
       <td className="text-center">
-      <input
-            type="checkbox"
-            aria-label="Amount of reps or seconds"
-            className="xt-check xt-switch rounded-full bg-gray-200 border border-transparent transition-all checked:bg-primary-500"
-            name="switch-usage"
-            onChange={ handleToggle }
-            checked={ timed }
+        <input
+          type="checkbox"
+          aria-label="Amount of reps or seconds"
+          className="xt-check xt-switch rounded-full bg-gray-200 border border-transparent transition-all checked:bg-primary-500"
+          name="switch-usage"
+          onChange={handleToggle}
+          checked={timed}
         />
       </td>
-      <td className="text-center"><button className="border px-1 rounded-full text-red-600 bg-white border-red-800 text-lg leading-5 align-text-top" onClick={ _ => props.onDelete(props.id) }>&times;</button></td>
+      <td className="text-center">
+        <button
+          className="border px-1 rounded-full text-red-600 bg-white border-red-800 text-lg leading-5 align-text-top"
+          onClick={(e) => props.onDelete(e, props.id)}
+        >
+          &times;
+        </button>
+      </td>
     </tr>
   );
-}
+};
 
 export default LapRow;
--- components/lapTable/LapTable.tsx
+++ components/lapTable/LapTable.tsx
@@ -1,5 +1,5 @@
 import { FC, SyntheticEvent, useEffect, useRef, useState } from "react";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import { Exercise, UsedExerciseArrayItem } from "../../types";
 import LapRow from "./LapRow";
 
@@ -10,45 +10,51 @@ type Props = {
   exerciseList: Exercise[];
   onUpdate(id: string, exercises: UsedExerciseArrayItem[]): void;
   onDelete(e: SyntheticEvent, id: string): void;
-}
+};
 
 const LapTable: FC<Props> = (props: Props) => {
   const [showSelect, setShowSelect] = useState(false);
-  const [selectedExercise, setSelectedExercise] = useState<Exercise | null>(null);
-  const [data, setData] = useState<UsedExerciseArrayItem[]>(props.exercises ?? []);
-  const isLoaded = useRef(false)
+  const [selectedExercise, setSelectedExercise] = useState<Exercise | null>(
+    null
+  );
+  const [data, setData] = useState<UsedExerciseArrayItem[]>(
+    props.exercises ?? []
+  );
+  const isLoaded = useRef(false);
 
   const handleUpdate = (id: string, reps: number, timed: boolean) => {
-    const newState = data.map(data => {
-      if (data.usedExercise.id === id) 
+    const newState = data.map((data) => {
+      if (data.usedExercise.id === id)
         return {
           usedExercise: {
             exercise: data.usedExercise.exercise,
             reps: reps,
             timed: timed,
-            id
-          }
-        }
+            id,
+          },
+        };
 
       return data;
     });
 
     setData(newState);
-  }
+  };
 
   const handleSelect = (e: SyntheticEvent) => {
     setSelectedExercise({
       id: (e.target as HTMLInputElement).value,
-      title: props.exerciseList.find(exercise => exercise.id === (e.target as HTMLInputElement).value)?.title ?? "Unknown exercise"
+      title:
+        props.exerciseList.find(
+          (exercise) => exercise.id === (e.target as HTMLInputElement).value
+        )?.title ?? "Unknown exercise",
     });
-  }
+  };
 
   const handleAddExerciseButton = (e: SyntheticEvent) => {
     e.preventDefault();
     setShowSelect(true);
 
-    if (selectedExercise === null)
-      return;
+    if (selectedExercise === null) return;
 
     const newExercise: UsedExerciseArrayItem = {
       usedExercise: {
@@ -56,77 +62,104 @@ const LapTable: FC<Props> = (props: Props) => {
         exercise: {
           id: selectedExercise.id,
           title: selectedExercise.title,
-          description: ""
+          description: "",
         },
         reps: 0,
-        timed: false
-      }
-    }
+        timed: false,
+      },
+    };
 
-    setData(prevData => ([...prevData, newExercise]));
-  }
+    setData((prevData) => [...prevData, newExercise]);
+  };
 
-  const handleDeleteButton = (id: string) => {
-    setData(data.filter((prevData) => prevData.usedExercise.id !== id))
-  }
+  const handleDeleteButton = (e: SyntheticEvent, id: string) => {
+    e.preventDefault();
+
+    setData(data.filter((prevData) => prevData.usedExercise.id !== id));
+  };
 
   useEffect(() => {
     if (isLoaded.current) {
-      props.onUpdate(props.id, data)
+      props.onUpdate(props.id, data);
     } else {
       isLoaded.current = true;
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [data]) 
+  }, [data]);
 
   return (
     <>
-      <label className="input-label" htmlFor="title">Exercises in lap #{ props.lapIndex + 1 }</label>
+      <label className="input-label" htmlFor="title">
+        Exercises in lap #{props.lapIndex + 1}
+      </label>
       <div className="block w-full">
         <table className="w-full bg-gray-50 text-gray-700 border border-slate-300 py-3 mb-3 leading-tight">
-        <thead className="text-xs border-b border-slate-300 rounded-tl-lg rounded-tr-lg">
-          <tr>
-            <th className="w-8/12 text-left pl-4 py-4">Exercise</th>
-            <th className="w-1/12 text-center py-4">Reps. / Sec.</th>
-            <th className="w-1/12 text-center py-4">Is timed</th>
-            <th className="w-2/12 text-center py-4">Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          { data && data.map((exercise: UsedExerciseArrayItem, i: number) => (
-            <LapRow id={ exercise.usedExercise.id } title={ exercise.usedExercise.exercise.title } reps={ exercise.usedExercise.reps ?? 0 } timed={ exercise.usedExercise.timed ?? false } onUpdate={ handleUpdate } onDelete={ handleDeleteButton } key={ i } /> 
-          )) }
-        </tbody>
-        <tfoot>
-          <tr>
-            <td className="py-4 pl-4" colSpan={3}>
-              {
-                showSelect &&
-                <div className="inline-block mr-5 border-gray-900">
-                  <select
-                    className="inline-block w-full xt-select rounded-md py-1 px-3.5 pr-20 text-gray-900 placeholder-black placeholder-opacity-75 bg-gray-100 transition focus:bg-gray-200 focus:outline-none"
-                    aria-label="Select"
-                    onChange={ handleSelect }
-                    defaultValue="selected"
-                  >
-                    <option value="selected" disabled>Select an option</option>
-                    { props.exerciseList.map((exercise: Exercise) => (
-                      <option value={ exercise.id } key={ exercise.id }>{ exercise.title }</option>
-                    )) }
-                  </select>
-                </div>
-              }
-              <button className="add-button-sm" onClick={ handleAddExerciseButton } disabled={ (showSelect && selectedExercise === null) }>Add{ !showSelect && " exercise" }</button>
-            </td>
-            <td className="py-4 text-center">
-              <button className="delete-button-sm" onClick={ e => props.onDelete(e, props.id) }>Remove lap</button>
-            </td>
-          </tr>
-        </tfoot>
-      </table>
-    </div>
-  </>
+          <thead className="text-xs border-b border-slate-300 rounded-tl-lg rounded-tr-lg">
+            <tr>
+              <th className="w-8/12 text-left pl-4 py-4">Exercise</th>
+              <th className="w-1/12 text-center py-4">Reps. / Sec.</th>
+              <th className="w-1/12 text-center py-4">Is timed</th>
+              <th className="w-2/12 text-center py-4">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data &&
+              data.map((exercise: UsedExerciseArrayItem, i: number) => (
+                <LapRow
+                  id={exercise.usedExercise.id}
+                  title={exercise.usedExercise.exercise.title}
+                  reps={exercise.usedExercise.reps ?? 0}
+                  timed={exercise.usedExercise.timed ?? false}
+                  onUpdate={handleUpdate}
+                  onDelete={handleDeleteButton}
+                  key={i}
+                />
+              ))}
+          </tbody>
+          <tfoot>
+            <tr>
+              <td className="py-4 pl-4" colSpan={3}>
+                {showSelect && (
+                  <div className="inline-block mr-5 border-gray-900">
+                    <select
+                      className="inline-block w-full xt-select rounded-md py-1 px-3.5 pr-20 text-gray-900 placeholder-black placeholder-opacity-75 bg-gray-100 transition focus:bg-gray-200 focus:outline-none"
+                      aria-label="Select"
+                      onChange={handleSelect}
+                      defaultValue="selected"
+                    >
+                      <option value="selected" disabled>
+                        Select an option
+                      </option>
+                      {props.exerciseList.map((exercise: Exercise) => (
+                        <option value={exercise.id} key={exercise.id}>
+                          {exercise.title}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                )}
+                <button
+                  className="add-button-sm"
+                  onClick={handleAddExerciseButton}
+                  disabled={showSelect && selectedExercise === null}
+                >
+                  Add{!showSelect && " exercise"}
+                </button>
+              </td>
+              <td className="py-4 text-center">
+                <button
+                  className="delete-button-sm"
+                  onClick={(e) => props.onDelete(e, props.id)}
+                >
+                  Remove lap
+                </button>
+              </td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+    </>
   );
-}
+};
 
 export default LapTable;
