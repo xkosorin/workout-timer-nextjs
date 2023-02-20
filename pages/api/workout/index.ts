8293f8f0ab358882e4dda819afbf54ed89ced420
Kristian Kosorin <kristian@kosorin.eu>
2023-02-20 11:37:09 +0100

    Added isPublic field to create workout API

--- pages/api/workout/index.ts
+++ pages/api/workout/index.ts
@@ -5,33 +5,38 @@ import { Lap, UsedExerciseArrayItem } from "../../../types";
 
 interface ExtendedNextApiRequest extends NextApiRequest {
   body: {
-    userId: string,
-    title: string,
-    description: string,
-    laps: Lap[],
+    userId: string;
+    title: string;
+    isPublic: boolean;
+    description: string;
+    laps: Lap[];
   };
 }
 
-export default async function handle(req: ExtendedNextApiRequest, res: NextApiResponse) {
-  const { userId, title, description, laps } = req.body;
+export default async function handle(
+  req: ExtendedNextApiRequest,
+  res: NextApiResponse
+) {
+  const { userId, title, isPublic, description, laps } = req.body;
 
   const session = await getSession({ req });
 
   if (!session) {
     res.status(403).json({
       message:
-        'You must be sign in to view the protected content on this page.',
-    })
+        "You must be signed in to view the protected content on this page.",
+    });
   }
 
   const result = await prisma.workout.create({
     data: {
-      user: {
+      createdBy: {
         connect: {
-          id: userId
-        }
+          id: userId,
+        },
       },
       title: title,
+      isPublic: isPublic,
       description: description,
       laps: {
         create: laps.map((lap) => ({
@@ -41,19 +46,19 @@ export default async function handle(req: ExtendedNextApiRequest, res: NextApiRe
                 create: {
                   exercise: {
                     connect: {
-                      id: exercise.usedExercise.exercise.id
-                    }
+                      id: exercise.usedExercise.exercise.id,
+                    },
                   },
                   reps: exercise.usedExercise.reps,
-                  timed: exercise.usedExercise.timed
-                }
-              }
-            }))
+                  timed: exercise.usedExercise.timed,
+                },
+              },
+            })),
           },
-          exerciseCount: lap.exercises.length
-        }))
-      }
-    }
+          exerciseCount: lap.exercises.length,
+        })),
+      },
+    },
   });
   res.status(200).json(result);
 }
