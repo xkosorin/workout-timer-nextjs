eba055181d4a9a7c625b2edb9ddf170bc93eb62e
Kristian Kosorin <kristian@kosorin.eu>
2023-03-09 10:27:51 +0100

    Update of tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,16 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    './node_modules/xtendui/src/*.mjs'
+    "./node_modules/tw-elements/dist/js/**/*.js",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
   presets: [
-    require('tailwindcss/defaultConfig'),
-    require('xtendui/tailwind.preset')
+    require("tailwindcss/defaultConfig"),
+    require("tw-elements/dist/plugin"),
+    require("flowbite/plugin"),
   ],
 };
