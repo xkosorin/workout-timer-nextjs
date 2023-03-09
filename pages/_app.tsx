8a7f0afd4715736c89851aa16a84f35e9b1197f1
Kristian Kosorin <kristian@kosorin.eu>
2023-03-09 10:34:27 +0100

    Update of _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,28 @@
-import "../styles/globals.css"; 
-import type { AppProps } from 'next/app'
-import { SessionProvider } from 'next-auth/react';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import { useEffect } from "react";
 
-function WorkoutTimer({ Component, pageProps }: AppProps) {
+function WorkoutTimer({
+  Component,
+  pageProps,
+}: AppProps<{
+  session: Session;
+}>) {
+  useEffect(() => {
+    const use = async () => {
+      // @ts-ignore
+      (await import("tw-elements")).default;
+    };
+    use();
+  }, []);
+
   return (
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
     </SessionProvider>
-  )
+  );
 }
 
-export default WorkoutTimer
+export default WorkoutTimer;
