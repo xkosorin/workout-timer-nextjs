94bbd01b77ee437a5d23afdc90917a405b3ba00b
Kristian Kosorin <kristian@kosorin.eu>
2023-03-09 16:51:16 +0100

    Added ability to display Youtube video in exercise detail

--- pages/exercise/[id]/index.tsx
+++ pages/exercise/[id]/index.tsx
@@ -1,18 +1,21 @@
 import { GetServerSideProps, NextPage } from "next";
 import { useSession } from "next-auth/react";
-import Image from 'next/image'
+import Image from "next/image";
 import Link from "next/link";
 import Router from "next/router";
 import { SyntheticEvent } from "react";
 import Layout from "../../../components/Layout";
-import prisma from '../../../lib/prisma'
-import { Exercise } from '../../../types';
+import prisma from "../../../lib/prisma";
+import { Exercise } from "../../../types";
+import { getYoutubeVideId } from "../../../utils/helpers";
 
 type Props = {
   exercise: Exercise;
-}
+};
 
-export const getServerSideProps: GetServerSideProps<any> = async ({ params }) => {
+export const getServerSideProps: GetServerSideProps<any> = async ({
+  params,
+}) => {
   const exercise = await prisma.exercise.findUnique({
     where: {
       id: String(params?.id),
@@ -21,55 +24,104 @@ export const getServerSideProps: GetServerSideProps<any> = async ({ params }) =>
       id: true,
       title: true,
       description: true,
-      mediaURL: true
-    }
+      mediaURL: true,
+      mediaIsImage: true,
+    },
   });
 
   return {
     props: {
-      exercise
-    }
-  }
-}
+      exercise,
+    },
+  };
+};
 
 const Exercise: NextPage<Props> = (props: Props) => {
   const { data: session, status } = useSession();
   let options;
-  
-  const handleDeleteButton = async(e: SyntheticEvent, id: string) => {
+
+  const handleDeleteButton = async (e: SyntheticEvent, id: string) => {
     e.preventDefault();
 
     try {
       await fetch(`/api/exercise/${id}`, {
-        method: 'DELETE',
+        method: "DELETE",
       });
-      await Router.push('/exercises');
+      await Router.push("/exercises");
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
   if (status === "authenticated") {
     if (session.user.role == "admin") {
-      options = <>
-        <Link href={ "/exercise/" + props.exercise.id + "/edit" }><a className="add-button mr-2">Edit exercise</a></Link>
-        <button onClick={ e => handleDeleteButton(e, props.exercise.id!) } className="delete-button">Delete exercise</button>
-      </>;
+      options = (
+        <>
+          <Link href={"/exercise/" + props.exercise.id + "/edit"}>
+            <a className="add-button mr-2">Edit exercise</a>
+          </Link>
+          <button
+            onClick={(e) => handleDeleteButton(e, props.exercise.id!)}
+            className="delete-button"
+          >
+            Delete exercise
+          </button>
+        </>
+      );
     }
   }
+
+  const getYoutubeLink = () => {
+    let videoId;
+
+    if (props.exercise.mediaURL) {
+      videoId = getYoutubeVideId(props.exercise.mediaURL);
+    }
+
+    return `https://www.youtube.com/embed/${videoId}`;
+  };
+
+  const getMedia = () => {
+    if (!props.exercise.mediaURL) {
+      return "https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg";
+    }
+
+    if (props.exercise.mediaIsImage) {
+      return (
+        <Image
+          src={props.exercise.mediaURL}
+          alt="Test"
+          width={600}
+          height={600}
+        />
+      );
+    } else {
+      return (
+        <iframe
+          id="ytplayer"
+          width="100%"
+          height="360"
+          src={`${getYoutubeLink()}`}
+          allow="autoplay; encrypted-media"
+          allowFullScreen
+          title="video"
+        />
+      );
+    }
+  };
 
   return (
     <Layout>
       <div className="grid grid-cols-1 md:grid-cols-2 px-2 md:px-0">
         <div className="flex flex-col md:flex-row md:col-span-2">
-          <h3 className="order-2 md:order-1">{ props.exercise.title }</h3>
-          <span className="ml-0 md:ml-auto order-1 md:order-2">{ options }</span>
+          <h3 className="order-2 md:order-1">{props.exercise.title}</h3>
+          <span className="ml-0 md:ml-auto order-1 md:order-2">{options}</span>
         </div>
-        <p>{ props.exercise.description }</p>
-        { props.exercise.mediaURL ? <Image src={ props.exercise.mediaURL } alt="Test" width={ 600 } height={ 600 } /> : "" }
+        <p>{props.exercise.description}</p>
+        {getMedia()}
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default Exercise
+export default Exercise;
